de1cf091775884ab4b4d529e305f09ef865cfbc4
Aleksey Nemiro <aleksey@kbyte.ru>
2023-06-08 20:27:41 +0300

    v1.4 (202306081)

--- src/TapPlayer.Maui/Services/AppInfo.cs
+++ src/TapPlayer.Maui/Services/AppInfo.cs
@@ -12,7 +12,7 @@ internal class AppInfo : IAppInfo
 
   public string VersionString => $"{Version.Major}.{Version.Minor} (build {Version.Build})";
 
-  public Version Version => new Version(1, 4, 20230608);
+  public Version Version => new Version(1, 4, 202306081);
 
   public string BuildString => "0";
 

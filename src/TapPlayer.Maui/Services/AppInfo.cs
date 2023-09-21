fd306cfbd7c9f4b3af479bfbcf67766b0243c768
Aleksey Nemiro <aleksey@kbyte.ru>
2023-09-21 22:37:28 +0300

    20230921 => 202309211

--- src/TapPlayer.Maui/Services/AppInfo.cs
+++ src/TapPlayer.Maui/Services/AppInfo.cs
@@ -12,7 +12,7 @@ internal class AppInfo : IAppInfo
 
   public string VersionString => $"{Version.Major}.{Version.Minor} (build {Version.Build})";
 
-  public Version Version => new Version(1, 5, 20230921);
+  public Version Version => new Version(1, 5, 202309211);
 
   public string BuildString => "0";
 
